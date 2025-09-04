190c3c571dd4779f291b8d6e7420317035c98d22
afoncom <afon.com12@gmail.com>
2025-09-04 21:44:27 +0300

    Initial Commit

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by afon.com on 04.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
