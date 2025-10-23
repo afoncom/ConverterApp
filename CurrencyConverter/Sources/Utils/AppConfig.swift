b70fc2e514e2a7fc14c69e05e7cc950ed652708b
afoncom <afon.com12@gmail.com>
2025-10-23 12:28:51 +0300

    change/structColor

--- CurrencyConverter/Sources/Utils/AppConfig.swift
+++ CurrencyConverter/Sources/Utils/AppConfig.swift
@@ -33,8 +33,6 @@ struct AppConfig {
     
     struct Colors {
         static let success = Color.green
-        static let warning = Color.orange
-        static let error = Color.red
         static let info = Color.blue
     }
     
