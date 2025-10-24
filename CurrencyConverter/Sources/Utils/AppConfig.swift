bbb786176caf4abe6ddba496f9f3fd37ab703b82
afoncom <afon.com12@gmail.com>
2025-10-24 17:23:52 +0300

    add CGFloat+extensions

--- CurrencyConverter/Sources/Utils/AppConfig.swift
+++ CurrencyConverter/Sources/Utils/AppConfig.swift
@@ -129,16 +129,3 @@ struct AppConfig {
         
     }
 }
-
-// MARK: - UI Size Extensions
-
-extension CGFloat {
-    /// Размеры (только используемые)
-    static let cornerRadius: CGFloat = 16
-    static let smallCornerRadius: CGFloat = 12
-    static let standardPadding: CGFloat = 16
-    static let smallPadding: CGFloat = 8
-    
-    /// Размеры компонентов
-    static let currencyButtonHeight: CGFloat = 80
-}
